605f4eb482a989e775d52444b220e40900cbbe30
左思成 <zsc32137@ly.com>
2019-01-22 19:34:04 +0800

    formate

--- redis-kill.sh
+++ redis-kill.sh
@@ -2,10 +2,10 @@
 #echo "usage: host port  ip/ip:port "
 
 if [[ ! -n "$1" ]] || [[ ! -n "$2" ]] || [[ ! -n "$3" ]]; then
-  echo "参数不合法！"
-  exit 2   
+    echo "参数不合法！"
+    exit 2   
 else
-  echo "start..."  
+    echo "start..."  
 fi
 
 all_count=$(redis-cli -h $1 -p $2 client list| awk 'END{print NR}')
@@ -14,15 +14,15 @@ target_count=$(redis-cli -h $1 -p $2 client list| awk '$2 ~ /'''$3'''/'| awk 'EN
 echo "该redis共有$all_count个客户端，其中目标客户端共有$target_count个，是否进行kill？(y/n)"
 read answer
 if [ "$answer" == "y" ]; then
-   list=$(redis-cli -h $1 -p $2 client list| awk '$2 ~ /'''$3'''/'| awk '{print substr($2,6)}')
+    list=$(redis-cli -h $1 -p $2 client list| awk '$2 ~ /'''$3'''/'| awk '{print substr($2,6)}')
 
-   for item in ${list[@]}; do
-   redis-cli -h $1 -p $2 client kill  $item
-   echo "kill client $item"
-   done
+    for item in ${list[@]}; do
+    redis-cli -h $1 -p $2 client kill  $item
+    echo "kill client $item"
+    done
 
-   echo "Done"
-   exit
+    echo "Done"
+    exit
 else
     echo "告辞！"
     exit
